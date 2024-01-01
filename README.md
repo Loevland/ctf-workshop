02b7f0b8aab10f73c0f18483311b1e82fb8d202e
Loevland <alo102@uit.no>
2024-01-01 14:04:51 +0100

    Overflow 2 chall

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Flaggformat: `TD{}`
 
 - Pwn:
   - [Overflow 1](pwn/overflow1)
+  - [Overflow 2](pwn/overflow2)
 
 - Rev:
 
---
+++ pwn/overflow2/src/vuln.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+void ignore_me(){
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void timeout(int signal){
+    if (signal == SIGALRM){
+        printf("You timed out!\n");
+        _exit(0);
+    }
+}
+
+void ignore_me_timeout(){
+    signal(SIGALRM, timeout);
+    alarm(60);
+}
+
+void print_flag(){
+    char chr;
+    FILE *f = fopen("flag.txt", "r");
+    chr = fgetc(f);
+    while (chr != EOF){
+        printf("%c", chr);
+        chr = fgetc(f);
+    }
+    printf("\n");
+    fclose(f);
+}
+
+void win(){
+    print_flag();
+}
+
+int main(){
+    ignore_me();
+    ignore_me_timeout();
+
+    char name[32];
+
+    puts("What is your name?");
+    gets(name);
+
+    return 0;
+}
